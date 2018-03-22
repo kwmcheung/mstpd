6a7e1ba0e168f3f39a628fce83e291844b0de3a3
Jonas Johansson <jonasj76@gmail.com>
2018-03-22 10:38:52 +0100

    snmp: add and compile with libnsh submodule
    Signed-off-by: Jonas Johansson <jonasj76@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+[ -e .gitmodules ] && [ -e .git ] && {
+    echo "autogen.sh: updating git submodules"
+    git submodule init
+    git submodule update
+}
+
 autoreconf -W portability -visf
